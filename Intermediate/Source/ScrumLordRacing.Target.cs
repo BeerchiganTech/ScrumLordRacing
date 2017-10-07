52dc7e477c609599c706ed5bc9516b271b594603
BeerchiganTech <31831422+BeerchiganTech@users.noreply.github.com>
2017-10-07 17:36:44 -0400

    Main Menu Updates

---
+++ Intermediate/Source/ScrumLordRacing.Target.cs
@@ -0,0 +1,26 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ScrumLordRacingTarget : TargetRules
+{
+    public ScrumLordRacingTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+
+        ExtraModuleNames.Add("UE4Game");
+	}
+
+	//
+	// TargetRules interface.
+	//
+
+	public override void SetupGlobalEnvironment(
+		TargetInfo Target,
+		ref LinkEnvironmentConfiguration OutLinkEnvironmentConfiguration,
+		ref CPPEnvironmentConfiguration OutCPPEnvironmentConfiguration
+		)
+	{
+	}
+}
